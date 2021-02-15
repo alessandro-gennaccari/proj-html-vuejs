1a437b8104bffe2ffb7b8affb0707be8ace94346
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-02-15 12:51:22 +0100

    created-header.navbar

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        header: [
+            {
+                logo: 'img/cropped-cropped-icon-270x270.png',
+                logoName1: 'Phlox',
+                logoName2: 'Corporate',
+                navMenu: [
+                    {
+                       name: 'Home',
+                       link: '#'
+                    },
+                    {
+                        name: 'About',
+                        link: '#'
+                     },
+                     {
+                        name: 'Service',
+                        link: '#'
+                     },
+                     {
+                        name: 'Blog',
+                        link: '#'
+                     },
+                     {
+                        name: 'Conctat',
+                        link: '#'
+                     },
+                     {
+                        name: 'Portfolio',
+                        link: '#'
+                     }
+                ],
+                sign: 'Sign In'
+            }
+        ]
+    }
+}); 

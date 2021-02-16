c772a0c90e0bae148a82b11f7ac70347e9e016c9
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2021-02-16 16:24:05 +0100

    added-footer

--- js/script.js
+++ js/script.js
@@ -38,6 +38,63 @@ var app = new Vue({
                 ],
                 sign: 'Sign In'
             }
+        ],
+        footer: [
+            {
+                logo: 'img/white-logo-2.png',
+                infoText: 'When, while lovely valley teems vapour aroundmeand meridian sun strikes the upper impenetrable',
+                quickLinks: [
+                    {
+                        title: 'Services',
+                        url: '#what'
+                    },
+                    {
+                        title: 'Contact',
+                        url: '#'
+                    },
+                    {
+                        title: 'Blog',
+                        url: '#post'
+                    }
+                ],
+                resources: [
+                    {
+                        title: 'Art Design',
+                        url: '#'
+                    },
+                    {
+                        title: 'Computer',
+                        url: '#'
+                    },
+                    {
+                        title: 'IT News',
+                        url: '#'
+                    },
+                    {
+                        title: 'Networking',
+                        url: '#'
+                    },
+                    {
+                        title: 'Web Security',
+                        url: '#'
+                    }
+                ],
+                contact: [
+                    {
+                        title: 'Address:',
+                        info: 'Patricia C. Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051'
+                    },
+                    {
+                        title: 'Phone:',
+                        info: '+99(0) 101 0000 888'
+                    },
+                    {
+                        title: 'Email:',
+                        info: 'Info@yourdomain.com'
+                    }
+                ]
+            }
+            
         ]
     },
     methods: {
